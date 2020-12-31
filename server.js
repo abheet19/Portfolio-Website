3fef329cf799333af7e7f6717414efeece4f665a
abheet19 <60404707+abheet19@users.noreply.github.com>
2020-12-31 22:01:40 +0530

    initial build v 1.0
    Signed-off-by: abheet19 <60404707+abheet19@users.noreply.github.com>

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app =express();
+
+const PORT= process.env.PORT || 4444
+
+app.use('/',express.static(__dirname + '/public'));
+
+app.listen(PORT,() =>{
+    console.log('server started ');
+})
