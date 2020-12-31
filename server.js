fa4f20c3514964ac19599f2e4c2f78220c87b204
abheet19 <60404707+abheet19@users.noreply.github.com>
2020-12-31 23:03:34 +0530

    initial build V 1.0
    Signed-off-by: abheet19 <60404707+abheet19@users.noreply.github.com>

--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require('express');
 
 const app =express();
 
-const PORT= process.env.PORT || 4444
+const PORT= process.env.PORT || 5000
 
 app.use('/',express.static(__dirname + '/public'));
 
-app.listen(PORT,() =>{
-    console.log('server started ');
-})
+app.listen(PORT);
