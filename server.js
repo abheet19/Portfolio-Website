99024714ed19237981783ecbaf80fa7d59e8fd75
abheet19 <60404707+abheet19@users.noreply.github.com>
2021-01-01 04:20:04 +0530

    initial build v1.0
    Signed-off-by: abheet19 <60404707+abheet19@users.noreply.github.com>

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 
-const app =express();
+const app = express();
 
-PORT= 3000;
+var port= 3000;
 
 app.use('/',express.static(__dirname + '/public'));
 
-app.listen(PORT,"0.0.0.0");
+app.listen(port,()=>{
+    console.log("listening on 3000");
+});
