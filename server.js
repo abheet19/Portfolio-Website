8c071aaaf2314548911274f3378d6984a8e8992f
abheet19 <60404707+abheet19@users.noreply.github.com>
2021-01-01 21:31:30 +0530

    updated server.js
    Signed-off-by: abheet19 <60404707+abheet19@users.noreply.github.com>

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 
 var port= 3000;
 
-app.use('/',express.static(__dirname + '/public'));
+app.use('/',express.static( './' + '/public'));
 
 app.listen(port,()=>{
     console.log("listening on 3000");
